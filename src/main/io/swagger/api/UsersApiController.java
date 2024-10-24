3df7630e2c60849f5740942f0f326461b1783724
momotofff <momotofff@mail.ru>
2024-10-24 22:16:11 +0400

    Update User and Task Controller. Added function in DatabaseManager

--- src/main/io/swagger/api/UsersApiController.java
+++ src/main/io/swagger/api/UsersApiController.java
@@ -50,12 +50,18 @@ public class UsersApiController implements UsersApi
 
         try
         {
-            return new ResponseEntity(databaseManager.getUsers(), HttpStatus.OK);
+            List<User> users = databaseManager.getUsers();
+
+            if (users.isEmpty())
+                return new ResponseEntity<List<User>>(HttpStatus.NO_CONTENT);
+
+            return new ResponseEntity<List<User>>(users, HttpStatus.OK);
         }
+
         catch (SQLException e)
         {
-            System.err.println("Error getting users list: " + e.getMessage());
-            // TODO: Return verbose error info instead of simple 500
+            log.error("Error getting users list: {}", e.getMessage(), e);
+
             return new ResponseEntity<List<User>>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
@@ -67,26 +73,16 @@ public class UsersApiController implements UsersApi
                                                      schema=@Schema())
                                           @Valid @RequestBody CreateUserRequest body)
     {
-        // TODO: Implement
         String accept = request.getHeader("Accept");
 
         if (accept != null && accept.contains("application/json"))
-        {
-            try
-            {
-                return new ResponseEntity<User>(objectMapper.readValue("{\n  \"role\" : \"editor\",\n  \"id\" : 1,\n  \"email\" : \"user@example.com\",\n  \"username\" : \"username\"\n}", User.class), HttpStatus.NOT_IMPLEMENTED);
-            }
-            catch (IOException e)
-            {
-                log.error("Couldn't serialize response for content type application/json", e);
-                return new ResponseEntity<User>(HttpStatus.INTERNAL_SERVER_ERROR);
-            }
-        }
+            return new ResponseEntity<User>(databaseManager.createUser(body.getUsername(), User.RoleEnum.USER.getValue(), body.getEmail()), HttpStatus.CREATED);
 
         return new ResponseEntity<User>(HttpStatus.NOT_IMPLEMENTED);
     }
 
-    public ResponseEntity<User> updateUserById(@Parameter(in = ParameterIn.PATH,
+    @PostMapping
+    public ResponseEntity<User> updateUserByName(@Parameter(in = ParameterIn.PATH,
                                                           description = "",
                                                           required=true,
                                                           schema=@Schema())
@@ -99,23 +95,18 @@ public class UsersApiController implements UsersApi
     {
         String accept = request.getHeader("Accept");
 
-        if (accept != null && accept.contains("application/json"))
-        {
-            try
-            {
-                return new ResponseEntity<User>(objectMapper.readValue("{\n  \"role\" : \"User\",\n  \"id\" : 1,\n  \"email\" : \"user@example.com\",\n  \"username\" : \"username\"\n}", User.class), HttpStatus.NOT_IMPLEMENTED);
-            }
-            catch (IOException e)
-            {
-                log.error("Couldn't serialize response for content type application/json", e);
-                return new ResponseEntity<User>(HttpStatus.INTERNAL_SERVER_ERROR);
-            }
-        }
+        if (accept == null || !accept.contains("application/json"))
+            return new ResponseEntity<>(HttpStatus.NOT_ACCEPTABLE);
 
-        return new ResponseEntity<User>(HttpStatus.NOT_IMPLEMENTED);
+        User updatedUser = databaseManager.updateUser(username, body);
+
+        if (updatedUser == null)
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+
+        return new ResponseEntity<>(updatedUser, HttpStatus.OK);
     }
 
-    public ResponseEntity<User> getUserById(@Parameter(in = ParameterIn.PATH,
+    public ResponseEntity<User> getUserByName(@Parameter(in = ParameterIn.PATH,
                                                        description = "",
                                                        required=true,
                                                        schema=@Schema())
